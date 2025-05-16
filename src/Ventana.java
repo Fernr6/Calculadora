9e60f1e6c87f1cd930cad0988837c9c3a14c8944
Fernr6 <fruizg1366@gmail.com>
2025-05-16 15:42:40 +0200

    Tercera entrega - Radio Buttons

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,10 +1,7 @@
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Image;
 import javax.swing.ButtonGroup;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -29,9 +26,9 @@ public class Ventana extends JFrame {
 
     private void initComponentes(){
         Paneles();
-        //Etiquetas();
+        Etiquetas();
         Botones();
-        //RadioBotones();
+        RadioBotones();
 
     }
     
@@ -45,14 +42,14 @@ public class Ventana extends JFrame {
     
     private void Etiquetas(){
 
-        JLabel etiqueta = new JLabel("Hola", SwingConstants.CENTER); //Crear etiqueta (texto, alineacion horizontal)
+        JLabel etiqueta = new JLabel("Lenguajes:", SwingConstants.CENTER); //Crear etiqueta (texto, alineacion horizontal)
     
-        etiqueta.setBounds(10,10,50,20); //Posicion de etiqueta
+        etiqueta.setBounds(40,80,100,20); //Posicion de etiqueta
     
-        etiqueta.setForeground(Color.WHITE); //Color de letra de etiqueta
-        etiqueta.setOpaque(true); //Establecer fondo de etiqueta
+        //etiqueta.setForeground(Color.BLACK); //Color de letra de etiqueta
+        etiqueta.setOpaque(false); //Establecer fondo de etiqueta
         etiqueta.setBackground(Color.BLACK); //Color de fondo de etiqueta
-        etiqueta.setFont(new Font("times new roman",Font.BOLD,30)); //Fuente de texto
+        //etiqueta.setFont(new Font("times new roman",Font.BOLD,30)); //Fuente de texto
     
         panel.add(etiqueta); //Agregar etiqueta
 
@@ -60,33 +57,24 @@ public class Ventana extends JFrame {
 
     private void Botones(){
         JButton boton1 = new JButton();
-        boton1.setText("Hola");
+        boton1.setText("Validar");
         boton1.setEnabled(true); //Controlar si el boton es utilizable
         boton1.setMnemonic('a'); //Utilizar boton con alt + 'a'
-        boton1.setBounds(200, 100, 80, 50); //Posicion y tamaño de boton
+        boton1.setBounds(50, 220, 80, 50); //Posicion y tamaño de boton
         boton1.setForeground(Color.BLUE);//Color de texto en boton
-        boton1.setFont(new Font("times new roman", 3, 20)); //Ajustes Font de texto del boton
-        
-        JButton boton2 = new JButton();
-        boton2.setText("Adios");
-        boton2.setEnabled(true);
-        boton2.setOpaque(true);
-        ImageIcon click = new ImageIcon("images.png");
-        boton2.setBounds(200, 200, 80, 50);
-        boton2.setIcon(new ImageIcon(click.getImage().getScaledInstance(boton2.getWidth(), boton2.getHeight(), Image.SCALE_SMOOTH)));
-        
+        //boton1.setFont(new Font("times new roman", 3, 20)); //Ajustes Font de texto del boton
+                
         panel.add(boton1);
-        panel.add(boton2);
     }
 
     private void RadioBotones(){
-        JRadioButton radio1 = new JRadioButton("Opcion 1", false);
+        JRadioButton radio1 = new JRadioButton("Java", false);
         radio1.setBounds(50,100,100,50);
 
-        JRadioButton radio2 = new JRadioButton("Opcion 2", false);
+        JRadioButton radio2 = new JRadioButton("PHP", false);
         radio2.setBounds(50,135,100,50);
 
-        JRadioButton radio3 = new JRadioButton("Opcion 3", false);
+        JRadioButton radio3 = new JRadioButton("C++", false);
         radio3.setBounds(50,165,100,50);
 
         panel.add(radio1);
